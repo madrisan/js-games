6b09d7d880205d4d6f702e4861bde9add2b9da0f
Davide Madrisan <davide.madrisan@gmail.com>
2016-08-17 20:28:34 +0200

    frogger: implement a basic Player class
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- frogger/js/app.js
+++ frogger/js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 100;
+    this.y = 100;
 };
 
 // Update the enemy's position, required method for game
@@ -21,20 +23,47 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 var Player = function() {
+    // FIXME: see engine.js
+    this.canvasRows = 6;
+    this.canvasCols = 5;
+    this.xOffset = 101;
+    this.yOffset = 83;
+
+    this.row = 5;
+    this.col = 2;
+    this.x = this.col * this.xOffset;
+    this.y = this.row * this.yOffset;
     this.sprite = 'images/char-boy.png';
 };
 
-Player.prototype.update = function(dt) {
+// Update the player's position, required method for game
+Player.prototype.update = function() {
+    this.x = this.col * this.xOffset;
+    this.y = this.row * this.yOffset;
 };
 
 Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function() {
+Player.prototype.handleInput = function(key) {
+    switch(key) {
+        case 'left':
+            this.col = Math.max(0, this.col-1);
+            break;
+        case 'up':
+            this.row = Math.max(0, this.row-1);
+            break;
+        case 'right':
+            this.col = Math.min(this.canvasCols-1, this.col+1);
+            break;
+        case 'down':
+            this.row = Math.min(this.canvasRows-1, this.row+1);
+            break;
+        default:
+            return
+    }
 };
 
 // Now instantiate your objects.
